7f83503fea1d27b416fb58b64c7875d56628284c
Fritz Obermeyer <fritzo@uber.com>
2019-05-14 16:56:44 -0700

    Add stepsize monitoring

--- Makefile
+++ Makefile
@@ -8,6 +8,9 @@ install: FORCE
 lint: FORCE
 	flake8
 
+test: FORCE lint
+	python main.py -n 1
+
 #clean: FORCE
 #	find . -name '*.pyc' -delete -o -name '*.log' -delete
 #	rm -f data/*.pkl model/*
--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def load_boston_housing(args):
 
 
 def load_census(args):
-    num_rows = min(2458286, args.max_num_rows)
+    num_rows = min(2458285, args.max_num_rows)
     filename = os.path.join(DATA, "census.{}.pkl".format(num_rows))
     if os.path.exists(filename):
         with open(filename, "rb") as f:
@@ -67,23 +67,19 @@ def load_census(args):
             num_cols = len(header)
             supports = [defaultdict(set) for _ in range(num_cols)]
             data = torch.zeros(num_rows, num_cols, dtype=torch.uint8)
-            mask = torch.zeros(num_rows, num_cols, dtype=torch.uint8)
             for i, row in enumerate(reader):
                 if i == num_rows:
                     break
                 for j, (cell, support) in enumerate(zip(row[1:], supports)):
-                    if cell:
-                        value = support.setdefault(cell, len(support))
-                        assert value <= 255
-                        data[i, j] = value
-                        mask[i, j] = 1
+                    value = support.setdefault(int(cell), len(support))
+                    assert value <= 255
+                    data[i, j] = value
 
         supports = [list(sorted(s)) for s in supports]
         dataset = {
             "header": header,
             "supports": supports,
             "data": data,
-            "mask": mask,
             "args": args,
         }
         with open(filename, "wb") as f:
@@ -91,7 +87,7 @@ def load_census(args):
     features = []
     data = []
     for j, support in enumerate(dataset["supports"]):
-        if len(support) >= 2 and dataset["mask"][:, j].all():
+        if len(support) >= 2:
             name = dataset["header"][j]
             features.append(Discrete(name, len(support)))
             data.append(dataset["data"][:, j].long().contiguous())
@@ -125,18 +121,37 @@ def printing_params(model):
     signal.signal(signal.SIGINT, signal.default_int_handler)
 
 
-def _random_walk(edges, decay=0.75):
-    V = len(edges) + 1
-    p = torch.eye(V)
-    p[edges[:, 0], edges[:, 1]] = 1
-    p[edges[:, 1], edges[:, 0]] = 1
-    return torch.inverse(torch.eye(V) - decay * p / p.sum(0)) * (1 - decay) / V
+class TreeMonitor(object):
+    def __init__(self, edges):
+        self.current = self._random_walk(edges)
+
+    def _random_walk(self, edges, decay=0.75):
+        """
+        Computes a heuristic distance between two trees.
+        """
+        V = len(edges) + 1
+        p = torch.eye(V)
+        p[edges[:, 0], edges[:, 1]] = 1
+        p[edges[:, 1], edges[:, 0]] = 1
+        return torch.inverse(torch.eye(V) - decay * p / p.sum(0)) * (1 - decay) / V
+
+    def get_diff(self, edges):
+        prev = self.current
+        self.current = self._random_walk(edges)
+        return (self.current - prev).abs().sum().item()
 
 
-def tree_distance(old_edges, new_edges):
-    old_dist = _random_walk(old_edges)
-    new_dist = _random_walk(new_edges)
-    return (old_dist - new_dist).abs().sum().item()
+class ParamStoreMonitor(object):
+    def __init__(self):
+        self.current = {}
+        self.get_diffs()
+
+    def get_diffs(self):
+        prev = self.current
+        self.current = {name: value.unconstrained().data.clone()
+                        for name, value in pyro.get_param_store().items()}
+        return {name: (curr - prev.get(name, 0)).norm(2).item()
+                for name, curr in self.current.items()}
 
 
 def main(args):
@@ -152,21 +167,25 @@ def main(args):
         num_rows, len(features), num_cells))
     logging.info("\n".join(["Features:"] + [str(f) for f in features]))
 
-    # Train a model.
+    # Initialize the model.
+    logging.debug("Initializing from {} rows".format(args.init_size))
     pyro.set_rng_seed(123456789)
     pyro.clear_param_store()
     pyro.enable_validation(__debug__)
-
-    model = TreeCat(features, args.capacity)
+    model = TreeCat(features, args.capacity, annealing_rate=args.annealing_rate)
     optim = Adam({"lr": args.learning_rate})
     trainer = TreeCatTrainer(model, optim, backend=args.backend)
     for batch in partition_data(data, args.init_size):
-        trainer.init(data)
+        trainer.init(batch)
         break
     print_params(model)
-    edges = model.edges
+
+    # Train a model.
+    logging.debug("Training for {} epochs".format(args.num_epochs))
+    tree_monitor = TreeMonitor(model.edges)
+    param_store_monitor = ParamStoreMonitor()
+    stepsizes = []
     losses = []
-    jumps = []
     with printing_params(model):
         for epoch in range(args.num_epochs):
             epoch_loss = 0
@@ -175,24 +194,23 @@ def main(args):
                 loss = trainer.step(batch, num_rows=num_rows)
                 loss /= num_cells
                 losses.append(loss)
-                jump = tree_distance(edges, model.edges)
-                edges = model.edges
-                jumps.append(jump)
-                logging.debug("  jump = {:0.3g}\tloss = {:0.3g}".format(jump, loss))
                 epoch_loss += loss
                 num_batches += 1
+
+                stepsize = param_store_monitor.get_diffs()
+                feature_stepsize = sum(stepsize.values())
+                stepsize["tree"] = tree_monitor.get_diff(model.edges)
+                stepsizes.append(stepsize)
+                logging.debug("tree_stepsize = {:0.4g}, feature_stepsize = {:0.4g}, loss = {:0.4g}".format(
+                    stepsize["tree"], feature_stepsize, loss))
             logging.info("epoch {} loss = {}".format(epoch, epoch_loss / num_batches))
 
-    # Save model and metadata.
+            # Save model and metadata.
+            pyro.get_param_store().save(os.path.join(RESULTS, "{}.model.pyro".format(args.dataset)))
+            meta = {"args": args, "losses": losses, "stepsizes": stepsizes}
+            with open(os.path.join(RESULTS, "{}.meta.pkl".format(args.dataset)), "wb") as f:
+                pickle.dump(meta, f, pickle.HIGHEST_PROTOCOL)
     print_params(model)
-    pyro.get_param_store().save(os.path.join(RESULTS, "model.pyro"))
-    meta = {
-        "args": args,
-        "losses": losses,
-        "jumps": jumps,
-    }
-    with open(os.path.join(RESULTS, "meta.pkl"), "wb") as f:
-        pickle.dump(meta, f, pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
@@ -202,10 +220,11 @@ if __name__ == "__main__":
     parser.add_argument("--max-num-rows", default=1000000000, type=int)
     parser.add_argument("--only-features")
     parser.add_argument("-c", "--capacity", default=8, type=int)
-    parser.add_argument("-lr", "--learning-rate", default=0.01, type=float)
+    parser.add_argument("-lr", "--learning-rate", default=0.04, type=float)
+    parser.add_argument("-ar", "--annealing-rate", default=0.01, type=float)
     parser.add_argument("-n", "--num-epochs", default=100, type=int)
     parser.add_argument("-b", "--batch-size", default=32, type=int)
-    parser.add_argument("-i", "--init-size", default=100, type=int)
+    parser.add_argument("-i", "--init-size", default=200, type=int)
     parser.add_argument("--backend", default="cpp")
     parser.add_argument("-v", "--verbose", action="store_true")
     args = parser.parse_args()
@@ -220,5 +239,6 @@ if __name__ == "__main__":
     try:
         main(args)
     except (ValueError, RuntimeError, AssertionError) as e:
+        print(e)
         import pdb
         pdb.post_mortem(e.__traceback__)
