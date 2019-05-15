84c584562e363281c66c97e4b774f195e9ece4e6
Fritz Obermeyer <fritzo@uber.com>
2019-05-14 17:46:06 -0700

    Split main.py into train.py + preprocess.py

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ lint: FORCE
 	flake8
 
 test: FORCE lint
-	python main.py -n 1
+	python train.py -n 1
 
 #clean: FORCE
 #	find . -name '*.pyc' -delete -o -name '*.log' -delete
--- train.py
+++ train.py
@@ -1,106 +1,22 @@
 from __future__ import absolute_import, division, print_function
 
 import argparse
-import csv
 import logging
 import os
 import signal
-from collections import defaultdict
 from contextlib import contextmanager
 
 import torch
-from observations import boston_housing
 from six.moves import cPickle as pickle
 
 import pyro
-from pyro.contrib.tabular import Boolean, Discrete, Real, TreeCat, TreeCatTrainer
+from preprocess import load_data, partition_data
+from pyro.contrib.tabular import TreeCat, TreeCatTrainer
 from pyro.contrib.tabular.treecat import print_tree
 from pyro.optim import Adam
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
-RAWDATA = os.path.join(ROOT, "rawdata")
-DATA = os.path.join(ROOT, "data")
 RESULTS = os.path.join(ROOT, "data")
-
-
-def load_data(args):
-    name = "load_{}".format(args.dataset)
-    assert name in globals()
-    return globals()[name](args)
-
-
-def load_boston_housing(args):
-    filename = os.path.join(DATA, "boston_housing.pkl")
-    if os.path.exists(filename):
-        with open(filename, "rb") as f:
-            dataset = pickle.load(f)
-    else:
-        x_train, metadata = boston_housing(DATA)
-        x_train = torch.tensor(x_train.T, dtype=torch.get_default_dtype()).contiguous()
-        features = []
-        data = []
-        logging.info("loaded {} rows x {} features:".format(x_train.size(1), x_train.size(0)))
-        for name, column in zip(metadata["columns"], x_train):
-            ftype = Boolean if name == "CHAS" else Real
-            features.append(ftype(name))
-            data.append(column)
-        dataset = {
-            "feature": features,
-            "data": data,
-            "args": args,
-        }
-        with open(filename, "wb") as f:
-            pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
-    return dataset["feature"], dataset["data"]
-
-
-def load_census(args):
-    num_rows = min(2458285, args.max_num_rows)
-    filename = os.path.join(DATA, "census.{}.pkl".format(num_rows))
-    if os.path.exists(filename):
-        with open(filename, "rb") as f:
-            dataset = pickle.load(f)
-    else:
-        with open(os.path.join(RAWDATA, "uci-us-census-1990", "USCensus1990.data.txt")) as f:
-            reader = csv.reader(f)
-            header = next(reader)[1:]
-            num_cols = len(header)
-            supports = [defaultdict(set) for _ in range(num_cols)]
-            data = torch.zeros(num_rows, num_cols, dtype=torch.uint8)
-            for i, row in enumerate(reader):
-                if i == num_rows:
-                    break
-                for j, (cell, support) in enumerate(zip(row[1:], supports)):
-                    value = support.setdefault(int(cell), len(support))
-                    assert value <= 255
-                    data[i, j] = value
-
-        supports = [list(sorted(s)) for s in supports]
-        dataset = {
-            "header": header,
-            "supports": supports,
-            "data": data,
-            "args": args,
-        }
-        with open(filename, "wb") as f:
-            pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
-    features = []
-    data = []
-    for j, support in enumerate(dataset["supports"]):
-        if len(support) >= 2:
-            name = dataset["header"][j]
-            features.append(Discrete(name, len(support)))
-            data.append(dataset["data"][:, j].long().contiguous())
-    return features, data
-
-
-def partition_data(data, target_size):
-    num_rows = len(data[0])
-    begin = 0
-    while begin < num_rows:
-        end = begin + target_size
-        yield [col[begin: end] for col in data]
-        begin = end
 
 
 def print_params(model):
