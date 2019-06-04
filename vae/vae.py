a52d1cfb2d13620ebcbbe6989aa9e01446abaaaf
jpchen <jonathanp.chen@gmail.com>
2019-06-04 16:33:22 -0700

    train on lending dataset

--- vae/vae.py
+++ vae/vae.py
@@ -61,42 +61,36 @@ class Encoder(nn.Module):
             layers.append(hidden_activation)
             previous_layer_size = layer_size
 
+        self.mu_layer = nn.Linear(z_dim, z_dim)
+        self.log_var_layer = nn.Linear(z_dim, z_dim)
+
         self.hidden_layers = nn.Sequential(*layers)
-
-    def forward(self, inputs):
-        return self.hidden_layers(inputs)
-
-
-class VAE(nn.Module):
-    def __init__(self, input_size, split_size, z_dim, encoder_hidden_sizes=[], decoder_hidden_sizes=[],
-                 variable_sizes=None, temperature=None):
-
-        super(VAE, self).__init__()
-
-        self.encoder = Encoder(input_size, split_size, hidden_sizes=encoder_hidden_sizes, variable_sizes=variable_sizes)
-        self.decoder = Decoder(z_dim, input_size, hidden_sizes=decoder_hidden_sizes, variable_sizes=variable_sizes,
-                               temperature=temperature)
-
-        self.mu_layer = nn.Linear(split_size, z_dim)
-        self.log_var_layer = nn.Linear(split_size, z_dim)
 
     def _reparameterize(self, mu, log_var):
         std = torch.exp(0.5 * log_var)
         eps = torch.randn_like(std)
         return eps.mul(std).add(mu)
 
+    def forward(self, inputs):
+        h = self.hidden_layers(inputs)
+        mu, log_var = self.mu_layer(h), self.log_var_layer(h)
+        return self._reparameterize(mu, log_var), mu, log_var
+
+
+class VAE(nn.Module):
+    def __init__(self, input_size, z_dim, encoder_hidden_sizes=[], decoder_hidden_sizes=[],
+                 variable_sizes=None, temperature=None):
+
+        super(VAE, self).__init__()
+
+        self.encoder = Encoder(input_size, z_dim, hidden_sizes=encoder_hidden_sizes, variable_sizes=variable_sizes)
+        self.decoder = Decoder(z_dim, input_size, hidden_sizes=decoder_hidden_sizes, variable_sizes=variable_sizes,
+                               temperature=temperature)
+
     def forward(self, inputs, training=False):
-        mu, log_var = self.encode(inputs)
-        code = self.reparameterize(mu, log_var)
-        reconstructed = self.decode(code, training=training)
-        return code, reconstructed, mu, log_var
-
-    def encode(self, inputs):
-        outputs = self.encoder(inputs)
-        return self.mu_layer(outputs), self.log_var_layer(outputs)
-
-    def decode(self, code, training=False):
-        return self.decoder(code, training=training)
+        z, mu, log_var = self.encoder(inputs)
+        reconstructed = self.decoder(z, training=training)
+        return z, reconstructed, mu, log_var
 
 
 def main(args):
@@ -110,7 +104,10 @@ def main(args):
     if args.multi:
         raise NotImplementedError('MultiInput/output')
     else:
-        vae = VAE(args.batch_size, 128, 128, [30, 15], [30, 15])
+        vae = VAE(len(features), args.hidden_dim, [700, 128], [700, len(features)])
+    if args.cuda:
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        vae.cuda()
     optim = Adam(vae.parameters(), lr=args.learning_rate)
     losses = []
     for i in range(args.num_epochs):
@@ -121,21 +118,37 @@ def main(args):
             if args.cuda and torch.cuda.is_available():
                 batch_data = to_cuda(batch_data)
                 batch_mask = to_cuda(batch_mask)
-            bb()
+            # preprocessing the data (TODO move this to preprocess.py)
+            for i, m in enumerate(batch_mask):
+                if isinstance(m, torch.Tensor):
+                    continue
+                if not m:
+                    # fill missing data with std noise
+                    batch_data[i] = torch.randn(args.batch_size)
+                    batch_mask[i] = torch.zeros(args.batch_size)
+                else:
+                    batch_mask[i] = torch.ones(args.batch_size)
+            batch_data = [torch.stack([x.float() for x in batch_data], -1)][0]
+            batch_mask = [torch.stack([x.float() for x in batch_mask], -1)][0]
             _, reconstructed, mu, log_var = vae(batch_data)
             # reconstruction loss + KLD
-            reconstruction_loss = reconstruction_loss_function(mask * reconstructed,
-                                                               mask * features,
-                                                               None,  # variable sizes
-                                                               reduction="sum") / torch.sum(mask)
+            reconstruction_loss = reconstruction_loss_function(batch_mask * reconstructed.clamp(min=0., max=1.),
+                                                               batch_mask * batch_data.clamp(min=0., max=1.),
+                                                               None,  # multioutput
+                                                               reduction="sum") / torch.sum(batch_mask)
 
-            kld_loss = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
-            loss = reconstruction_loss + kld_loss
+            kld = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+            loss = reconstruction_loss + kld
             loss.backward()
             optim.step()
             losses.append(loss)
             epoch_loss += loss
             num_batches += 1
+            if i % args.logging_interval == 0:
+                logging.info('[batch {}/{}]: loss = {}'
+                             .format((num_batches * args.batch_size),
+                                     data.shape[0],
+                                     epoch_loss))
 
 
 if __name__ == "__main__":
@@ -150,9 +163,14 @@ if __name__ == "__main__":
     parser.add_argument("-lr", "--learning-rate", default=0.01, type=float)
     parser.add_argument("-n", "--num-epochs", default=200, type=int)
     parser.add_argument("-b", "--batch-size", default=64, type=int)
+    parser.add_argument("--hidden-dim", default=128, type=int)
     parser.add_argument("--cuda", action="store_true", default=False)
     parser.add_argument("--multi", action="store_true", default=False,
                         help="whether to use multi input/output per Camino et al (2018)")
     parser.add_argument("-i", "--init-size", default=1000000000, type=int)
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-l", "--logging-interval", default=10, type=int)
     args = parser.parse_args()
+    logging.basicConfig(format="%(relativeCreated) 9d %(message)s",
+                        level=logging.DEBUG if args.verbose else logging.INFO)
     main(args)
